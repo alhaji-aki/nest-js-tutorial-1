e0e46b3c4eaca10fc99460a83c95f59e64e4192d
Alhaji Aki <abdulkudus2922@gmail.com>
2022-12-23 06:26:55 +0000

    finished dependency injection

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable({})
+export class AuthService {
+    signup(){
+        return {
+            message: 'I am sign up'
+        }
+    }
+
+    signin(){
+        return {
+            message: 'I am sign in'
+        }
+    }
+}
